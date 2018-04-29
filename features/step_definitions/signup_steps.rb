dcb92c29ac6069ff8884e17c9a67b4180d675c7d
JulijaTDL <julija.gruznova@testdevlab.com>
2018-04-29 20:31:40 +0300

    HW4

--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -18,15 +18,11 @@ Then("I enter {string}, {string} and {string} in Sign Up form") do |email, passw
   find(:css, 'input[name="password1"]').send_keys(password)
   find(:css, 'input[name="password2"]').send_keys(password)
   find(:css, 'input[name="project_name"]').send_keys(name)
-  sleep(1)
 end
 
 Then("I close Sign Up") do
   find(:css, '#signup .closecross').click
-  #find(:css, '#signup').should_not be_visible
-  unless find(:css, '#signup').visible?
-    raise "Element is not visible"
-  end
+  find(:css, '#signup').should_not be_visible
 end
 
 
@@ -42,7 +38,6 @@ end
 Then("I enter invalid information {string} and {string}") do |login, password|
   find(:css, '#login input[name="login"]').send_keys(login)
   find(:css, 'input[name="password"]').send_keys(password)
-  sleep(1)
 end
 
 Then("I try to login") do
